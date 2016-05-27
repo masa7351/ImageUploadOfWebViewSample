909fce7ec926e91eb70679d5bb5d0c4115b2e9a0
Masanao Imai <masa7351@gmail.com>
2016-05-27 14:19:40 +0900

    XWalkViewでカメラ撮影でアップロードできるように対応

--- app/src/main/java/info/meetmeet/imageuploadofwebviewsample/Main3Activity.java
+++ app/src/main/java/info/meetmeet/imageuploadofwebviewsample/Main3Activity.java
@@ -3,12 +3,19 @@ package info.meetmeet.imageuploadofwebviewsample;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.webkit.ValueCallback;
 
 import org.xwalk.core.XWalkUIClient;
 import org.xwalk.core.XWalkView;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class Main3Activity extends AppCompatActivity {
 
@@ -18,7 +25,7 @@ public class Main3Activity extends AppCompatActivity {
     private static final String TYPE_IMAGE = "image/*";
     private static final int INPUT_FILE_REQUEST_CODE = 1;
     private ValueCallback<Uri> mUploadMessage;
-
+    private String mCameraPhotoPath;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,10 +34,12 @@ public class Main3Activity extends AppCompatActivity {
 
 //        XWalkView view = new XWalkView(this, this);
         mXWalkWebView = (XWalkView)findViewById(R.id.webView);
-        mXWalkWebView.setUIClient(MyWebChromeClient(mXWalkWebView));
+//        mXWalkWebView.setUIClient(MyWebChromeClient(mXWalkWebView));
+        mXWalkWebView.setUIClient(CustomWebChromeClient(mXWalkWebView));
 
         mXWalkWebView.load("http://49.212.135.31/image_upload/edit_profile.html", null);
 //        mXWalkWebView.load("http://49.212.135.31/image_upload/sample.html", null);
+//        mXWalkWebView.load("http://code.matty-studio.jp/demo/HTML5/fileapi/preview.html", null);
     }
 
     @Override protected void onActivityResult(int requestCode, int resultCode, Intent data) {
@@ -43,8 +52,14 @@ public class Main3Activity extends AppCompatActivity {
         Uri result = null;
 
         if (resultCode == RESULT_OK) {
-            if (data != null) {
+            if (data != null && data.getData() != null) {
                 result = data.getData();
+            } else {
+                // If there is not data, then we may have taken a photo
+                if (mCameraPhotoPath != null) {
+                    result = Uri.parse(mCameraPhotoPath);
+                }
+
             }
         }
 
@@ -78,8 +93,22 @@ public class Main3Activity extends AppCompatActivity {
         }
     }
 
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = Environment.getExternalStoragePublicDirectory(
+                Environment.DIRECTORY_PICTURES);
+        File imageFile = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+        return imageFile;
+    }
+
     // https://crosswalk-project.org/apis/embeddingapidocs_v2/org/xwalk/core/XWalkUIClient.html
-    private XWalkUIClient MyWebChromeClient(XWalkView view) {
+    private XWalkUIClient CustomWebChromeClient(XWalkView view) {
         return new XWalkUIClient(view) {
             @Override
             public void openFileChooser(XWalkView view, ValueCallback<Uri> uploadFile, String acceptType, String capture) {
@@ -90,12 +119,48 @@ public class Main3Activity extends AppCompatActivity {
                 }
                 mUploadMessage = uploadFile;
 
-                Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
-                intent.addCategory(Intent.CATEGORY_OPENABLE);
-                intent.setType(TYPE_IMAGE);
+                Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+                    // Create the File where the photo should go
+                    File photoFile = null;
+                    try {
+                        photoFile = createImageFile();
+                        takePictureIntent.putExtra("PhotoPath", mCameraPhotoPath);
+                    } catch (IOException ex) {
+                        // Error occurred while creating the File
+                        Log.e(TAG, "Unable to create Image File", ex);
+                    }
 
-                startActivityForResult(intent, INPUT_FILE_REQUEST_CODE);
+                    // Continue only if the File was successfully created
+                    if (photoFile != null) {
+                        mCameraPhotoPath = "file:" + photoFile.getAbsolutePath();
+                        takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT,
+                                Uri.fromFile(photoFile));
+                    } else {
+                        takePictureIntent = null;
+                    }
+                }
+
+                Intent contentSelectionIntent = new Intent(Intent.ACTION_GET_CONTENT);
+                contentSelectionIntent.addCategory(Intent.CATEGORY_OPENABLE);
+                contentSelectionIntent.setType("image/*");
+
+                Intent[] intentArray;
+                if (takePictureIntent != null) {
+                    intentArray = new Intent[]{takePictureIntent};
+                } else {
+                    intentArray = new Intent[0];
+                }
+
+                Intent chooserIntent = new Intent(Intent.ACTION_CHOOSER);
+                chooserIntent.putExtra(Intent.EXTRA_INTENT, contentSelectionIntent);
+                chooserIntent.putExtra(Intent.EXTRA_TITLE, "Image Chooser");
+                chooserIntent.putExtra(Intent.EXTRA_INITIAL_INTENTS, intentArray);
+
+                startActivityForResult(chooserIntent, INPUT_FILE_REQUEST_CODE);
+
             }
         };
     }
+
 }
